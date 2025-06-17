254bb663053ea0796387e6a008161919a0189a84
aglaia <aglaia.hub@gmail.com>
2025-06-17 17:07:15 +0300

    CORS

--- device-service-app/src/main/java/rent/vehicle/deviceserviceapp/config/ServiceConfig.java
+++ device-service-app/src/main/java/rent/vehicle/deviceserviceapp/config/ServiceConfig.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServiceConfig {
@@ -16,6 +18,16 @@ public class ServiceConfig {
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 
